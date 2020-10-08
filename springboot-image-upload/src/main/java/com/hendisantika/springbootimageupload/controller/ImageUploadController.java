70f2b7078a252c435969c50035cbb51c7b21b4ba
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-09 05:23:10 +0700

    #12 - Add ImageUploadController

---
+++ springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootimageupload.controller;
+
+import com.hendisantika.springbootimageupload.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-image-upload
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/20
+ * Time: 05.22
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "image")
+public class ImageUploadController {
+
+    @Autowired
+    private ImageRepository imageRepository;
+}
