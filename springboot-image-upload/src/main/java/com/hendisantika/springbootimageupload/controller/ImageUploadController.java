61fb2e89cf5f0c54c4d2628a12d46fea0cbd8953
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-09 05:25:54 +0700

    #14 - Add uploadImage API in ImageUploadController

--- springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
+++ springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
@@ -1,11 +1,19 @@
 package com.hendisantika.springbootimageupload.controller;
 
+import com.hendisantika.springbootimageupload.entity.ImageModel;
 import com.hendisantika.springbootimageupload.repository.ImageRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +33,13 @@ public class ImageUploadController {
     @Autowired
     private ImageRepository imageRepository;
 
+    @PostMapping("/upload")
+    public ResponseEntity.BodyBuilder uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
 
+        log.info("Original Image Byte Size - " + file.getBytes().length);
+        ImageModel img = new ImageModel(file.getOriginalFilename(), file.getContentType(),
+                compressBytes(file.getBytes()));
+        imageRepository.save(img);
+        return ResponseEntity.status(HttpStatus.OK);
+    }
+}
