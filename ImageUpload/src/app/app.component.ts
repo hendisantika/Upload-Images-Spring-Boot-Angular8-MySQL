8eaf8017791fb1b3f3ed2cea7b1b56b6f84fd049
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-08 17:00:30 +0700

    #5 - Add onUpload in app.component.ts in Angular 10 Project

--- ImageUpload/src/app/app.component.ts
+++ ImageUpload/src/app/app.component.ts
@@ -21,4 +21,26 @@ export class AppComponent {
     //Select File
     this.selectedFile = event.target.files[0];
   }
+
+  //Gets called when the user clicks on submit to upload the image
+  onUpload() {
+    console.log(this.selectedFile);
+
+    //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    //Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('http://localhost:8080/image/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+          if (response.status === 200) {
+            this.message = 'Image uploaded successfully';
+          } else {
+            this.message = 'Image not uploaded successfully';
+          }
+        }
+      );
+
+
+  }
 }
