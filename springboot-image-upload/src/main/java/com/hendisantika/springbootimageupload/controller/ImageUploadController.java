ff26d1f103b955da0542419b5786a7112655418b
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-09 05:25:17 +0700

    #13 - Add ImageUploadController

--- springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
+++ springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootimageupload.controller;
 
 import com.hendisantika.springbootimageupload.repository.ImageRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 09/10/20
  * Time: 05.22
  */
+@Log4j2
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "image")
@@ -22,4 +24,5 @@ public class ImageUploadController {
 
     @Autowired
     private ImageRepository imageRepository;
-}
+
+
