c968592cb5c09367d4f5f9ad84ec05e985b6c757
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-09 05:27:39 +0700

    #16 - Add compressBytes method in ImageUploadController

--- springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
+++ springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.zip.Deflater;
 
 /**
  * Created by IntelliJ IDEA.
@@ -53,5 +55,26 @@ public class ImageUploadController {
         ImageModel img = new ImageModel(retrievedImage.get().getName(), retrievedImage.get().getType(),
                 decompressBytes(retrievedImage.get().getPicByte()));
         return img;
+    }
+
+    // compress the image bytes before storing it in the database
+    public static byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+
+        return outputStream.toByteArray();
     }
 }
