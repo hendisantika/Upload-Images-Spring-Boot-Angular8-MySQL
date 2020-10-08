6719bc60329040f235d618db6aec3fc3f2092039
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-09 05:26:44 +0700

    #15 - Add getImage API in ImageUploadController

--- springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
+++ springboot-image-upload/src/main/java/com/hendisantika/springbootimageupload/controller/ImageUploadController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -41,5 +44,14 @@ public class ImageUploadController {
                 compressBytes(file.getBytes()));
         imageRepository.save(img);
         return ResponseEntity.status(HttpStatus.OK);
+    }
+
+    @GetMapping(path = {"/get/{imageName}"})
+    public ImageModel getImage(@PathVariable("imageName") String imageName) throws IOException {
+
+        final Optional<ImageModel> retrievedImage = imageRepository.findByName(imageName);
+        ImageModel img = new ImageModel(retrievedImage.get().getName(), retrievedImage.get().getType(),
+                decompressBytes(retrievedImage.get().getPicByte()));
+        return img;
     }
 }
