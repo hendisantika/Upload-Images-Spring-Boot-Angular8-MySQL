c0152f6b2064882eda5a8c4ac24f27ad8018896c
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-08 17:13:31 +0700

    #9 - Add Spring Boot Project

---
+++ springboot-image-upload/src/test/java/com/hendisantika/springbootimageupload/SpringbootImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
